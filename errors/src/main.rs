eaf02f4b1f09eb2ffb71da3f850d179e69c9cf4e
luisurrea2016# <luis.urrea@motortrak.com>
2018-07-28 13:03:59 +0100

    follow Generics lesson

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -90,7 +90,7 @@ pub struct Guess {
 }
 
 impl Guess {
-    pub fn new(value:cu32) -> Guess {
+    pub fn new(value:u32) -> Guess {
         if value< 1 || value > 100 {
             panic!("Guess value must be between 1 and 100, got {}", value);
         }
---
+++ generics/src/main.rs
@@ -0,0 +1,191 @@
+use std::fmt::Display;
+
+fn main() {
+    // 
+    
+    // let p = Point { x: 5, y: 10 };
+
+    // println!("p.x = {}", p.x());
+
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c'};
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from("The Pittsburgh Penguins once again are the best
+        hockey team in the NHL."),
+    };
+
+    println!("New article available! {}", article.summarize());
+
+    let string1 = String::from("long string is long");
+    let string2 = String::from("abc");
+    let result = longest(string1.as_str(), string2.as_str());
+
+    println!("Longest string is {}", result);
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.')
+        .next()
+        .expect("Could not find a '.'");
+    let importance = ImportantExcerpt { part: first_sentence };
+
+    //println!("importance is {}", importance);
+
+}
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest2<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+
+// impl<T> Point<T> {
+//     fn x(&self) -> &T {
+//         &self.x
+//     }
+// }
+
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+//Traits are similar to a feature often called interfaces in other languages, 
+// although with some differences.
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+// impl Summary for NewsArticle {
+//     fn summarize(&self) -> String {
+//         format!("{}, by {} ({})", self.headline, self.author, self.location)
+//     }
+// }
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+// Default implementation
+pub trait Summary2 {
+    fn summarize(&self) -> String {
+        String::from("(Read more...)")
+    }
+}
+
+impl Summary2 for NewsArticle {
+    // add code here
+}
+
+//trait bounds
+pub fn notify<T: Summary>(item: T) {
+    println!("Breaking news! {}",item.summarize());
+}
+
+//fn some_function<T: Display + Clone, U: Clone + Debug>(t: T, u: U) -> i32 {
+    //or
+//fn some_function<T, U>(t: T, u: U) -> i32
+//     where T: Display + Clone,
+//           U: Clone + Debug
+// {
+
+
+//lifetime
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+// srtruct can hold references but they need lifetime annotations
+// https://doc.rust-lang.org/book/second-edition/ch10-03-lifetime-syntax.html#lifetime-annotations-in-struct-definitions
+#[derive(Debug)]
+struct ImportantExcerpt<'b> {
+    part: &'b str,
+}
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+    where T: Display
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
